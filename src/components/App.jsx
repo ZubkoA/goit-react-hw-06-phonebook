018c6cf81a5397d3c26ce312cf68825700266607
ZubkoA <114995045+ZubkoA@users.noreply.github.com>
2023-06-10 12:04:46 +0300

    comit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { nanoid } from 'nanoid';
+
 import Header from './Header';
-import { useState, useEffect } from 'react';
+
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import AddContacts from './AddContacts/AddContacts';
